3f07d99b3ba4a35598aa216bd965d9df1fa6650f
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2025-06-19 18:40:28 +0300

    RCA-11 добавлены DTO модели

---
+++ app/src/main/java/com/sol/recipecomposeapp/data/model/CategoryDto.kt
@@ -0,0 +1,8 @@
+package com.sol.recipecomposeapp.data.model
+
+data class CategoryDto(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imgUrl: String,
+)
---
+++ app/src/main/java/com/sol/recipecomposeapp/data/model/IngredientDto.kt
@@ -0,0 +1,7 @@
+package com.sol.recipecomposeapp.data.model
+
+data class IngredientDto(
+    val quantity: String,
+    val unitOfMeasure: String,
+    val description: String,
+)
